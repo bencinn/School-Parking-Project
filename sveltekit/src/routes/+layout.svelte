7ab4694e50c05e364d401bf33309d847cfd6f22c
hansnnn <pp.rongpan@gmail.com>
2023-05-17 16:08:34 +0700

    adding sveltekit

---
+++ sveltekit/src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+  import './app.css'
+</script>
+
+    <head>
+        <meta charset="UTF-8">
+        <title>ระบบจอดรถภูเก็ตวิทยาลัย</title>
+    </head>
+        <div id="webcontainer">
+            <div id="title">
+                <h1>ระบบจอดรถภูเก็ตวิทยาลัย</h1> 
+                <img src="/pkw.png">
+            </div>
+            <hr>
+            <div id="display">
+<slot />
+            </div>
+            </div>

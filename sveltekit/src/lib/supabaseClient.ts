7e617685b7821e8c3e3b896668ed882ba493de31
hansnnn <pp.rongpan@gmail.com>
2023-05-20 09:17:00 +0700

    Formatting stuff

--- sveltekit/src/lib/supabaseClient.ts
+++ sveltekit/src/lib/supabaseClient.ts
@@ -1,3 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 // @ts-ignore
-export const supabase_admin = createClient('https://qkdxcckaitrppzeduhnl.supabase.co', import.meta.env.VITE_SUPABASE_ADMIN_KEY);
+export const supabase_admin = createClient(
+	'https://qkdxcckaitrppzeduhnl.supabase.co',
+	import.meta.env.VITE_SUPABASE_ADMIN_KEY
+);
--- sveltekit/src/routes/+layout.svelte
+++ sveltekit/src/routes/+layout.svelte
@@ -1,18 +1,18 @@
 <script>
-  import './app.css'
+	import './app.css';
 </script>
 
-    <head>
-        <meta charset="UTF-8">
-        <title>ระบบจอดรถภูเก็ตวิทยาลัย</title>
-    </head>
-        <div id="webcontainer">
-            <div id="title">
-                <h1>ระบบจอดรถภูเก็ตวิทยาลัย</h1> 
-                <img src="/pkw.png">
-            </div>
-            <hr>
-            <div id="display">
-<slot />
-            </div>
-            </div>
+<head>
+	<meta charset="UTF-8" />
+	<title>ระบบจอดรถภูเก็ตวิทยาลัย</title>
+</head>
+<div id="webcontainer">
+	<div id="title">
+		<h1>ระบบจอดรถภูเก็ตวิทยาลัย</h1>
+		<img src="/pkw.png" />
+	</div>
+	<hr />
+	<div id="display">
+		<slot />
+	</div>
+</div>

2fc7c7794de14756f565ad5a0bf372440a32f5ab
ItsPungpond98 <pongspanichl@gmail.com>
2023-05-24 17:25:12 +0700

    Add Admin UI

---
+++ sveltekit/src/routes/admin/+page.server.js
@@ -0,0 +1,32 @@
+import { supabase_admin } from '$lib/supabaseClient';
+
+export async function load({ params }) {
+	const { data } = await supabase_admin.from('Parking_lot').select('parked_where');
+	return {
+		Parking_lot: data ?? [],
+		slug: params.slug
+	};
+}
+
+/** @type {import('./$types').Actions} */
+export const actions = {
+	default: async ({ cookies, request }) => {
+		const formdata = await request.formData();
+		const { data, error } = await supabase_admin.from('Parking_lot').insert([
+			{
+				parked_where: formdata.get('whereis'),
+				parker_name: formdata.get('name'),
+				parker_surname: formdata.get('surname'),
+				parker_handler: formdata.get('handler'),
+				position: formdata.get('position'),
+				phone_number: formdata.get('phone')
+			}
+		]);
+		console.log(error);
+		if (error === null) {
+			return { success: true };
+		} else {
+			return { success: false };
+		}
+	}
+};
--- sveltekit/src/routes/parking/[slug]/+page.svelte
+++ sveltekit/src/routes/parking/[slug]/+page.svelte
@@ -72,15 +72,5 @@
 			<input type="submit" id="btn" value="ลงทะเบียนใช้ที่จอดรถ" />
 		</div>
 	</form>
-	<fieldset style="margin-top: 10px;">
-		<legend>ที่จอดรถที่ได้ถูกใช้แล้ว</legend>
-		<ul style="color: white; margin: 0 0 10px 0; padding-left: 5px; list-style-type: none;">
-			{#each Parking_lot as parked}
-				<li style="color: white; margin-top: 10px;">
-					ช่องจอดรถที่ {parked.parked_where}
-					<a data-sveltekit-reload href={'/parking/' + parked.parked_where} id="btn" style="font-size: 15px; padding: 5px;">ออกจากระบบ</a>
-				</li>
-			{/each}
-		</ul>
-	</fieldset>
+
 {/if}

333639a2b884836d6966b878371d4204354bb560
hansnnn <pp.rongpan@gmail.com>
2023-06-17 20:15:19 +0700

    Moving to Typescript

--- sveltekit/src/routes/+page.server.js
+++ sveltekit/src/routes/+page.server.js
@@ -1,9 +1,9 @@
-import { supabase_admin } from '$lib/supabaseClient';
+import { database } from '$lib/supabaseClient';
 
 export async function load({ params }) {
-	const { data } = await supabase_admin.from('Parking_lot').select('parked_where');
-	return {
-		Parking_lot: data ?? [],
-		slug: params.slug
-	};
+  const { data } = await database.from('Parking_lot').select('parked_where');
+  return {
+    Parking_lot: data ?? [],
+    slug: params.slug
+  };
 }

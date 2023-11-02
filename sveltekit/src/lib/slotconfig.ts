5655d79a2acf1222385d613577dfbe8910e42315
ItsPungpond98 <pongspanichl@gmail.com>
2023-11-02 20:23:31 +0700

    Added Slot Config File

---
+++ sveltekit/src/lib/slotconfig.ts
@@ -0,0 +1,12 @@
+export const ParkingZone = [
+    {
+        name: "Building 1",
+        slot: [1, 2, 3, 4, 5, 6],
+    }, {
+        name: "Building 2",
+        slot: [7, 8, 9, 10, 11, 12],
+    }, {
+        name: "Building 3",
+        slot: [13, 14, 15, 16, 17, 18],
+    }
+];
